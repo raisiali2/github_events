227318f1c9e6824e80d84c10812b2c29ae300f97
a923304 <mohammad-ali.raisi@eviden.com>
2024-03-23 11:45:53 +0100

    added second comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 // my test
+// my second test
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
